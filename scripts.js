f425ca6607fcc982c37804b8d633a00fec04608a
anchoragewonder <anchoragewonder@gmail.com>
2020-09-09 21:00:07 -0400

    Initital javascript api test

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
+// return JSON data from any file path (asynchronous)
+function getJSON(path) {
+    return fetch(path).then(response => response.json());
+}
+
+// load JSON data; then proceed
+getJSON('https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.json').then(data => {
+    eplMatchlist = data;
+}
+
+console.log(eplMatchlist);
 
