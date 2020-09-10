496ead53659e0cbbf5edd43d7fa6034c9a8cca3d
anchoragewonder <anchoragewonder@gmail.com>
2020-09-09 21:14:14 -0400

    defining variables is important
    created a variable for json data and added script to html to reas scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+var eplMatchlist;
+
 // return JSON data from any file path (asynchronous)
 function getJSON(path) {
     return fetch(path).then(response => response.json());
@@ -6,7 +8,6 @@ function getJSON(path) {
 // load JSON data; then proceed
 getJSON('https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.json').then(data => {
     eplMatchlist = data;
-}
-
-console.log(eplMatchlist);
+    }
+);
 
