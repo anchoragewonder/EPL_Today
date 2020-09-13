2d788bfcbb0e945be71a89f30f4b85b93e5f7699
DanFlannel <dan.flanagan93@gmail.com>
2020-09-13 19:01:25 -0400

    creating object for json

--- scripts.js
+++ scripts.js
@@ -1,26 +1,49 @@
 var eplMatchlist;
+var dict;
+
+
+
+
+
 
 // return JSON data from any file path (asynchronous)
-function getJSON(path) {
-    return fetch(path).then(response => response.json());
+async function getJSON(path) {
+    let json = await fetch(path).then(response => response.json());
+    createJSObject(json);
 }
+
+function createJSObject(json) {
+
+    dict = new Object();
+
+    for (let i = 0; i < json.rounds.length; i++) {
+        let matches = json.rounds[i].matches;
+
+
+        for (let j = 0; j < matches.length; j++) {
+            let curMatch = matches[j];
+            let homeTeam = curMatch.team1;
+            let awayTeam = curMatch.team2;
+
+
+            if (!(homeTeam in dict)) {
+                dict[homeTeam] = [];
+            }
+
+            if (!(awayTeam in dict)) {
+                dict[awayTeam] = [];
+            }
+
+            dict[homeTeam].push(curMatch);
+            dict[awayTeam].push(curMatch);
+        }
+    }
+}
+
+
 
 // load JSON data; then proceed
 getJSON('https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.json').then(data => {
     eplMatchlist = data;
-    }
-);
-
-// access the json arrays to get home and away teams also date
-for(let i = 0, l = eplMatchlist.rounds.length; i < l; i++) {
-   let matchday = eplMatchlist.rounds[i];
-   for (let j = 0, k= matchday.length; j < k; j++){
-        let games = matchday.matches[j]
-        let day= matchday.name
-        for (let m = 0, p = games.length; m < p; m++){
-            let homeTeam = games.team1[m]
-            let awayTeam = games.team2[m]
-            let gameday = games.date[m]
-        }
-    }
 }
+);
